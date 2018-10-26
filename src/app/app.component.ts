39dc83d1438faab4a2b5f64b2d5f05d597f78496
Alina <kittine@yandex.ru>
2018-10-26 17:22:14 +0300

    Demo uploadcare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  yourOnChangeHandler = (e) => {
+    alert('Privet')
+  }
+
   title = 'uploadcare';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UcWidgetModule } from 'ngx-uploadcare-widget';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UcWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
